d1e835479d2eeb16fcfd7f3df9f9864159e9d357
christopher.tanaka <christopher.tanaka@c6bank.com>
2020-04-06 23:49:48 -0300

    Trabalhando na tela de Login e Home do App. Esboços iniciais.

--- MyApp/MyApp/ViewController.swift
+++ MyApp/MyApp/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usernameTxtField: UITextField!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
